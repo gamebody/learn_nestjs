f23f96b1ed86668b5a7573a0de0ad404c7d24b2e
liuyun <liuyun-pxb@sinosig.com>
2019-12-20 17:35:40 +0800

    更新api

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Patch, Post, Body, Delete, Param, Type } from '@nestjs/common';
 import { CreateTodoDto, UpdateTodoDto } from './dto/create-todo.dto';
 import { TodosService } from './todos.service';
-import { ApiUseTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiUseTags, ApiResponse, ApiOperation, ApiOkResponse, ApiImplicitParam } from '@nestjs/swagger';
 import { Todo } from './todo.entity';
 
 @ApiUseTags ('todos')
@@ -11,60 +11,30 @@ export class TodosController {
     constructor(private readonly todosService: TodosService) {}
     
     @ApiOperation({ title: "获取全部的Todo" })
-    @ApiResponse({ status: 200, description: '获取全部的todo2', type: [Todo]})
+    @ApiOkResponse({ description: '获取全部的todo2', type: [Todo]})
     @Get()
     async findAll(): Promise<Todo[]> {
         return this.todosService.findAll()
     }
 
     @ApiOperation({ title: "创建todo1" })
+    @ApiOkResponse({ description: '创建成功的todo的返回类型', type: Todo})
     @Post()
-    async create(@Body() CreateTodoDto: CreateTodoDto) {
-        const res = await this.todosService.create(CreateTodoDto)
-        return {
-            code: 1,
-            data: res,
-            message: '成功'
-        }
+    async create(@Body() CreateTodoDto: CreateTodoDto): Promise<Todo> {
+        return this.todosService.create(CreateTodoDto)
     }
 
     @ApiOperation({ title: "更新todo" })
+    @ApiOkResponse({ description: '更新的todo的返回类型', type: Todo})
     @Patch(':id')
     async update(@Param('id') id: number, @Body() UpdateTodoDto: UpdateTodoDto) {
-        try {
-            await this.todosService.update(id, UpdateTodoDto.completed);
-            return {
-                code: 1,
-                data: null,
-                message: "更新成功"
-            }
-        } catch (error) {
-            return {
-                code: 2,
-                data: error.message,
-                message: "更新失败"
-                
-            }
-        }
+        return this.todosService.update(id, UpdateTodoDto.completed);
     }
 
     @ApiOperation({ title: "删除todo" })
+    @ApiOkResponse({ description: '删除的todo的返回类型', type: Todo})
     @Delete(':id')
     async remove(@Param('id') id: number) {
-        try {
-            await this.todosService.delete(id)
-            return {
-                code: 1,
-                data: null,
-                message: "删除成功"
-            }
-        } catch (error) {
-            return {
-                code: 2,
-                data: error.message,
-                message: "删除失败"
-            }
-        }
+        return this.todosService.delete(id)
     }
-
 }
